9d1fa4ead15a0888a4df165709d4800f153b62cf
bernevek <bernevek1995@ukr.net>
2018-02-07 19:14:22 +0200

    ivan add 3 tasks 86a 86b 330

---
+++ tasks/TestIvan330.java
@@ -0,0 +1,33 @@
+package com.lv297java.hometask1.tasks;
+
+import java.util.Scanner;
+
+/**
+ * Created by ivan on 06.02.18.
+ */
+public class TestIvan330 extends AbstractTest {
+    public TestIvan330() {
+        super("TestIvan330");
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("enter your number");
+        Scanner scanner = new Scanner(System.in);
+        int var = scanner.nextInt();
+        if (var>0){
+            for (int i = 1; i < var; i++){
+                int sum = 0;
+                for (int j = 1; j < i; j++){
+                    if (i%j == 0)
+                        sum = sum + j;
+                }
+                if (i == sum)
+                    System.out.println("the number " + i +" is perfect");
+            }
+        }else {
+            System.out.println("the number is not natural");
+        }
+        System.out.println("*******************************************");
+    }
+}
---
+++ tasks/TestIvan86b.java
@@ -0,0 +1,31 @@
+package com.lv297java.hometask1.tasks;
+
+import java.util.Scanner;
+
+/**
+ * Created by ivan on 06.02.18.
+ */
+public class TestIvan86b extends AbstractTest {
+    public TestIvan86b() {
+        super("TestIvan86b");
+    }
+
+    @Override
+    public void execute() {
+        System.out.println("enter your number");
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int var = scanner.nextInt();
+        int var2 = var;
+        if (var>0){
+            while(var > 0){
+                sum += var%10;
+                var/=10;
+            }
+            System.out.println("the sum of digits number's "+var2+" are " +sum);
+        }else {
+            System.out.println("the number is not natural");
+        }
+        System.out.println("*******************************************");
+    }
+}
