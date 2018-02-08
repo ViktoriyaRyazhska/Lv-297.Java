7d524c951a71f41c59e4a6403b382a8775489011
Oksana Mekh <o.v.mekh@gmail.com>
2018-02-08 18:21:35 +0200

    Documented and check-styled AbstractTest and package-info.

--- src/com/lv297java/tasks/AbstractTest.java
+++ src/com/lv297java/tasks/AbstractTest.java
@@ -1,16 +1,40 @@
 package com.lv297java.tasks;
 
+/**
+ * Base class for all tests.
+ *
+ * @version 1.0 6 Feb 2018
+ * @author Ostap Kravtsiv
+ */
 public abstract class AbstractTest {
 
-    public AbstractTest(String testID){
+    /**
+     * Unique ID of test.
+     */
+    private String testID;
+
+    /**
+     * Initializes a newly created {@code AbstractTest} object so that it represents
+     * certain math test.
+     * @param testID unique ID of test
+     */
+    public AbstractTest(final String testID){
         this.testID = testID;
     }
 
-    private String testID;
-
+    /**
+     * Returns unique ID of current test.
+     *
+     * @return current {@link AbstractTest#testID}
+     */
     public String getTestID() {
         return testID;
     }
 
+    /**
+     * This method executes current test.
+     * It accepts data from the standard input (if necessary)
+     * and outputs the result to a standard output.
+     */
     public abstract void execute();
 }
