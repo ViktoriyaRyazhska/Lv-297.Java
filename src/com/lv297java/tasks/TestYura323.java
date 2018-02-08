817d3dea770e98f07c5caa8b62c6bcbde5d1e8c3
Yura Katola <yurakatola@gmail.com>
2018-02-08 20:10:42 +0200

    Documentation was add for 87, 559, 323

--- src/com/lv297java/tasks/TestYura323.java
+++ src/com/lv297java/tasks/TestYura323.java
@@ -1,38 +1,55 @@
 package com.lv297java.tasks;
 
 import java.util.Scanner;
-
-public class TestYura323 extends AbstractTest{
-	
-	private int n;
-	private Scanner sc;
-	
-	public TestYura323() {
-		super("323(3)");
-		sc = new Scanner(System.in);
-	}
-
-	@Override
-	public void execute() {
-		System.out.println("Naturally prime number n is given. Find all naturally prime numbers " + 
-				" smaller than n and relative prime with n.");
-		
-		System.out.println("Enter the naturally number n: ");
-		n = sc.nextInt();
-		
-		for (int i = 2; i < n; i++) {
-			if(gcd(n, i) == 1)
-				System.out.print(i + " ");
-		}
-	}
-	
-	private int gcd(int a,int b) {
-        while (b !=0) {
-            int tmp = a % b;
-            a = b;
-            b = tmp;
+/**
+ * Class that extends abstract class AbstractTest and
+ * contains solution for
+ * task number 323.
+ *
+ * @version 1.0
+ * @author Yura Katola
+ */
+public class TestYura323 extends AbstractTest {
+    /**
+     *
+     */
+    public TestYura323() {
+        super("323(3)");
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute() {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Naturally prime number n is given. Find all"
+                + " naturally prime numbers "
+                + " smaller than n and relative prime with n.");
+        System.out.println("Enter the naturally number n: ");
+        n = sc.nextInt();
+        for (int i = 2; i < n; i++) {
+            if (grandCentralDispatch(n, i) == 1) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+    /**
+     * This method returns GCD(Grand Central Dispatch) value
+     * of parameters a and b.
+     * @param a - first number, divided.
+     * @param b - second number, divider.
+     * @return grand central dispatch value of two numbers.
+     */
+    private int grandCentralDispatch(final int a, final int b) {
+        int divided = a;
+        int divider = b;
+        while (divider != 0) {
+            int tmp = divided % b;
+            divided = divider;
+            divider = tmp;
         }
         return a;
     }
-	// second commit
+    // second commit
 }
--- src/com/lv297java/tasks/TestYura87.java
+++ src/com/lv297java/tasks/TestYura87.java
@@ -1,38 +1,56 @@
 package com.lv297java.tasks;
 
 import java.util.Scanner;
-
-public class TestYura87 extends AbstractTest{
-	
-	private int n;
-	private int m;
-	private Scanner sc;
-	
-	public TestYura87() {
-		super("87(2)");
-		sc = new Scanner(System.in);
-	}
-
-	@Override
-	public void execute() {
-		System.out.println("Naturally numbers n, m is given. Find m last digits sum of number n.");
-		
-		System.out.println("Enter the naturally number n: ");
-		n = sc.nextInt();
-		
-		System.out.println("Enter the naturally number m: ");
-		m = sc.nextInt();
-		
-		int sum = 0;
-		
-		while(m > 0) {
-			sum += n % 10;
-			n = n / 10;
-			m--;
-		}
-		
-		System.out.println("Last digits sum of number n = " + sum );
-		
-		}
-	
+/**
+ * Class that extends abstract class AbstractTest and
+ * contains solution for
+ * task number 87.
+ *
+ * @version 1.0
+ * @author Yura Katola
+ */
+public class TestYura87 extends AbstractTest {
+    /**
+     * Initializes a newly created {@code TestYura87} object.
+     */
+    public TestYura87() {
+        super("87(2)");
+    }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void execute() {
+        int n;
+        int m;
+        int lastDigitsSum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Naturally numbers n, m is given. "
+                + "Find m last digits sum of number n.");
+        System.out.println("Enter the naturally number n: ");
+        n = sc.nextInt();
+        System.out.println("Enter the naturally number m: ");
+        m = sc.nextInt();
+        lastDigitsSum = findLastDigits(n, m);
+        System.out.println("Last digits sum of number n = "
+        + lastDigitsSum);
+        }
+    /**
+     * @param n - number which summary  of last digits we want
+     * to find.
+     * @param m - number that represents amount of last digits we want to sum.
+     * @return int value - summary m last digits of number n.
+     */
+    private int findLastDigits(final int n, final int m) {
+        int sum = 0;
+        int number = n;
+        int lastDigitAmount = m;
+        final int divider = 10;
+        while (m > 0) {
+            sum += number % divider;
+            number = number / divider;
+            lastDigitAmount--;
+        }
+        return sum;
+    }
 }
