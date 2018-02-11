218086dc0660dab0ee0ecd14eed9e0b428064ec9
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-02-11 13:49:20 +0200

    Added test dummies for tasks 87, 323, 559.

---
+++ test/com/lv297java/tasks/TaskYura87Test.java
@@ -0,0 +1,12 @@
+package com.lv297java.tasks;
+
+import org.junit.Test;
+
+public class TaskYura87Test {
+
+    @Test
+    public void testFindLastDigits() {
+        //TODO Write this test
+    }
+
+}
