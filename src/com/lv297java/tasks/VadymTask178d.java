47033fe83bea88291dbb77c260273b474fc4e14e
Vadym <t.vadyk@gmail.com>
2018-02-11 18:00:43 +0200

    Add Javadocs in VadymTask files

--- src/com/lv297java/tasks/VadymTask178d.java
+++ src/com/lv297java/tasks/VadymTask178d.java
@@ -1,34 +1,54 @@
 package com.lv297java.tasks;
 
-
 import com.lv297java.AbstractTask;
 import static com.lv297java.inputreader.BufferReader.reader;
 
 import java.io.IOException;
 
+/**
+ * With this class you can find amount of a.k elements in 'a.1,...,a.n that satisfy the condition like '2^k < a.k < k!'.
+ *
+ * @version 1.0
+ * @since 2017-11-02
+ * @author Vadym Teslytskyy
+ */
 public class VadymTask178d extends AbstractTask {
+    /**
+     * Entered progresion.
+     */
+    private String inputProgresion;
+    /**
+     * Progresion splited in array .
+     */
+    private String[] splitedInputProgresion;
+    /**
+     * String progresion array converted in int array .
+     */
+    private int[] progresionIntArr;
 
-	public VadymTask178d() {
-		super("178d");
-	}
-
-	private String inputProgresion;
-
-	private String[] splitedInputProgresion;
-
-	private int[] progresionIntArr;
-
-	private void progresionReading() {
-		System.out.println(
-				"Task is to find amount of a.k elements in 'a.1,...,a.n' that satisfy the condition like '2^k < a.k < k!'");
-		System.out.println("Enter number of progresion by space:");
-		try {
-			inputProgresion = reader.readLine();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
+    /**
+    * Initializes a newly created {@link VadymTask178d} object.
+    * It represents a Task 178d.
+    */
+    public VadymTask178d() {
+        super("178d");
+    }
+    /**
+     * This method reads a inputProgresion.
+     */
+    private void progresionReading() {
+        System.out.println("Task is to find amount of a.k elements in 'a.1,...,a.n' "
+            + "that satisfy the condition like '2^k < a.k < k!'");
+        System.out.println("Enter number of progresion by space:");
+        try {
+            inputProgresion = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /**
+     * This method covert String[] splitedInputProgresion to int[] progresionIntArr.
+     */
 	private void progresionConverting() {
 		splitedInputProgresion = inputProgresion.split("\\s+");
 		progresionIntArr = new int[splitedInputProgresion.length];
@@ -36,7 +56,11 @@ public class VadymTask178d extends AbstractTask {
 			progresionIntArr[i] = Integer.valueOf(splitedInputProgresion[i]);
 		}
 	}
-
+	/**
+     * This method calculate amount of number in progresionIntArr that satisfy the condition like '2^k < a.k < k!'.
+     * @param arrOfProg it`s int array of progresion in which we search numbers.
+     * @return counter = amount of numbers that satisfy the condition like '2^k < a.k < k!'.
+     */
 	public int calculation(int[] arrOfProg) {
 		int counter = 0;
 		int factorialOfCurrentStepCounter;
@@ -44,27 +68,38 @@ public class VadymTask178d extends AbstractTask {
 		for (int i = 0; i < arrOfProg.length; i++) {
 			factorialOfCurrentStepCounter = factorialCalculation(i);
 			twoInStepOfCurrentStepCounter = Math.pow(2, i);
-			if ((arrOfProg[i] < factorialOfCurrentStepCounter) && (arrOfProg[i] > twoInStepOfCurrentStepCounter)) {
+			if ((arrOfProg[i] < factorialOfCurrentStepCounter)
+					&& (arrOfProg[i] > twoInStepOfCurrentStepCounter)) {
 				counter++;
 			}
 		}
 		return counter;
 	}
-
-	public int factorialCalculation(int x) {
+	/**
+     * This method calculate factorial of number.
+     * @param baseNumber the number from which we calculate the factorial.
+     * @return result = result of factorial.
+     */
+	public final int factorialCalculation(int baseNumber) {
 		int result = 1;
-		for (int i = 1; i <= x; i++) {
+		for (int i = 1; i <= baseNumber; i++) {
 			result *= i;
 		}
 		return result;
 	}
-
+	/**
+     * This method print result of calculating.
+     * @param counter amount of numbers that satisfy the condition like '2^k < a.k < k!'.
+     */
 	private void printResult(int counter) {
-		System.out.println("Amount of elements that satisfy the condition like '2^k < a.k < k!' is -> " + counter);
+		System.out.println(
+				"Amount of elements that satisfy the condition like '2^k < a.k < k!' is -> " + counter);
 	}
-
+	/**
+     * {@inheritDoc}
+     */
 	@Override
-	public void execute() {
+	public final void execute() {
 		progresionReading();
 		progresionConverting();
 		printResult(calculation(progresionIntArr));
--- src/com/lv297java/tasks/VadymTask178g.java
+++ src/com/lv297java/tasks/VadymTask178g.java
@@ -4,20 +4,43 @@ import com.lv297java.AbstractTask;
 import static com.lv297java.inputreader.BufferReader.reader;
 
 import java.io.IOException;
-
+/**
+ * With this class you can find amount of a.k elements in 'a.1,...,a.n
+ * that satisfy the condition like 'a.k = (a.k-1+a.k+1)/2'.
+ *
+ * @version 1.0
+ * @since 2017-11-02
+ * @author Vadym Teslytskyy
+ */
 public class VadymTask178g extends AbstractTask {
 
-	private String inputProgresion;
-	private String[] splitedInputProgresion;
-	private int[] progresionIntArr;
+	/**
+     * Entered progresion.
+     */
+    private String inputProgresion;
+    /**
+     * Progresion splited in array .
+     */
+    private String[] splitedInputProgresion;
+    /**
+     * String progresion array converted in int array .
+     */
+    private int[] progresionIntArr;
 
+    /**
+    * Initializes a newly created {@link VadymTask178g} object.
+    * It represents a Task 178g.
+    */
 	public VadymTask178g() {
 		super("178g");
 	}
-
+	/**
+     * This method reads a inputProgresion.
+     */
 	private void progresionReading() {
 		System.out.println(
-				"Task is to find amount of a.k elements in 'a.1,...,a.n' that satisfy the condition like 'a.k = (a.k-1+a.k+1)/2'");
+				"Task is to find amount of a.k elements in 'a.1,...,a.n' "
+						+ "that satisfy the condition like 'a.k = (a.k-1+a.k+1)/2'");
 		System.out.println("Enter number of progresion by space:");
 		try {
 			inputProgresion = reader.readLine();
@@ -25,7 +48,9 @@ public class VadymTask178g extends AbstractTask {
 			e.printStackTrace();
 		}
 	}
-
+	/**
+     * This method covert String[] splitedInputProgresion to int[] progresionIntArr.
+     */
 	private void progresionConverting() {
 		splitedInputProgresion = inputProgresion.split("\\s+");
 		progresionIntArr = new int[splitedInputProgresion.length];
@@ -33,7 +58,13 @@ public class VadymTask178g extends AbstractTask {
 			progresionIntArr[i] = Integer.valueOf(splitedInputProgresion[i]);
 		}
 	}
-
+	/**
+     * This method calculate amount of number in progresionIntArr
+     * that satisfy the condition like 'a.k = (a.k-1+a.k+1)/2'.
+     *
+     * @param arrOfProg it`s int array of progresion in which we search numbers.
+     * @return counter = amount of numbers that satisfy the condition like 'a.k = (a.k-1+a.k+1)/2'.
+     */
 	public int calculation(int[] arrOfProg) {
 		int counter = 0;
 		for (int i = 1; i < arrOfProg.length - 1; i++) {
@@ -43,12 +74,17 @@ public class VadymTask178g extends AbstractTask {
 		}
 		return counter;
 	}
-
+	/**
+     * This method print result of calculating.
+     * @param counter amount of numbers that satisfy the condition like 'a.k = (a.k-1+a.k+1)/2'.
+     */
 	private void printResult(int counter) {
-		System.out
-				.println("Amount of elements that satisfy the condition like 'a.k = (a.k-1+a.k+1)/2' is -> " + counter);
+		System.out.println("Amount of elements that satisfy the condition like 'a.k = (a.k-1+a.k+1)/2' is -> "
+						+ counter);
 	}
-
+	/**
+     * {@inheritDoc}
+     */
 	@Override
 	public void execute() {
 		progresionReading();
