e104f6921ac4f98f291410b7cbf9815022c82783
Yura Katola <yurakatola@gmail.com>
2018-02-12 12:11:48 +0200

    Test classes 108 325 561 was added

---
+++ test/com/lv297java/tasks/TaskOleksiy108Test.java
@@ -0,0 +1,14 @@
+package com.lv297java.tasks;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TaskOleksiy108Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
