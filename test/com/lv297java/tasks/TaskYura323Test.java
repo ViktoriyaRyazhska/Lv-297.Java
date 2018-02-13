d479f75dd23bcbdef3e9af00ad4caf5e3d28521f
Nazariy Demyanovskyi <nazariy.demyanovskyi@gmail.com>
2018-02-13 13:00:47 +0200

    Added test for tasks 87, 323, 559.

--- test/com/lv297java/tasks/TaskYura323Test.java
+++ test/com/lv297java/tasks/TaskYura323Test.java
@@ -1,12 +1,37 @@
 package com.lv297java.tasks;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class TaskYura323Test {
 
+    private final YuraTask323 task = new YuraTask323();
+
+    @Test
+    public void testGrandCentralDispatchFromPositiveAndPositive() {
+        Assert.assertEquals(5, task.grandCentralDispatch(25, 15));
+    }
+
+    @Test
+    public void testGrandCentralDispatchFromPositiveAndNegative() {
+        Assert.assertEquals(-5, task.grandCentralDispatch(-25, 15));
+    }
+
+    @Test
+    public void testGrandCentralDispatchFromNegativeAndPositive() {
+        Assert.assertEquals(5, task.grandCentralDispatch(-25, 15));
+    }
+
+    @Test
+    public void testGrandCentralDispatchFromNegativeAndNegative() {
+        Assert.assertEquals(-5, task.grandCentralDispatch(-25, -15));
+    }
+
     @Test
     public void testFindNaturallyPrimes() {
-        //TODO Write this test
+        Assert.assertEquals(Arrays.asList(8, 7, 5, 4, 2, 1), task.findNaturallyPrimes(9));
     }
 
 }
--- test/com/lv297java/tasks/TaskYura559Test.java
+++ test/com/lv297java/tasks/TaskYura559Test.java
@@ -1,12 +1,22 @@
 package com.lv297java.tasks;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class TaskYura559Test {
 
+    private final YuraTask559 task = new YuraTask559();
+
     @Test
-    public void testFindMersenNumbers() {
-        //TODO Write this test
+    public void testFindMersenNumbersPositive() {
+        Assert.assertEquals(Arrays.asList(1, 3, 7), task.findMersenNumbers(9));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testFindMersenNumbersNegative() {
+        task.findMersenNumbers(-1);
     }
 
 }
